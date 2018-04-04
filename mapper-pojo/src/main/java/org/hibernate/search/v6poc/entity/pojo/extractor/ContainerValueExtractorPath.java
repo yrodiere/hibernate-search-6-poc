2134ab1e5dfac5869f03814695fd189514e8e04b
Yoann Rodière <yoann@hibernate.org>
2018-04-04 18:31:02 +0200

    Move ContainerValueExtractorPath to API and use it in programmatic mapping
    We will need it to model complex paths when modeling inverse side of
    associations.
    
    Using it in the programmatic mapping is unrelated, but seems to make
    sense.

--- mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/extractor/ContainerValueExtractorPath.java
+++ mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/extractor/ContainerValueExtractorPath.java
@@ -4,14 +4,12 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.search.v6poc.entity.pojo.extractor.spi;
+package org.hibernate.search.v6poc.entity.pojo.extractor;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-
-import org.hibernate.search.v6poc.entity.pojo.extractor.ContainerValueExtractor;
 
 public class ContainerValueExtractorPath {
 
